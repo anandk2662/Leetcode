d7ee48f1bce5ac36f0073a0cb39ac724dee1510f
anandk2662 <130994394+anandk2662@users.noreply.github.com>
2025-12-29 22:04:00 +0530

    Time: 16 ms (17.93%), Space: 13.5 MB (64.33%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -5,8 +5,6 @@ class Solution(object):
         :rtype: None Do not return anything, modify nums in-place instead.
         """
         for i in range(len(nums)-1,-1,-1):
-            if len(nums)==1:
-                return nums
-            elif nums[i]==0:
+            if nums[i]==0:
                 nums.append(nums.pop(i))
         return nums
