d0f787d5f08b1d535edb85856a9be983876434c8
anandk2662 <130994394+anandk2662@users.noreply.github.com>
2026-01-13 10:15:30 +0530

    Time: 15 ms (76.73%), Space: 26 MB (22.04%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -4,10 +4,8 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        freq={}
-        for i in nums:
-            freq[i]=freq.get(i,0)+1
-        if len(freq.keys())!=len(nums):
-            return (True)
+        unique=len(set(nums))
+        if unique!=len(nums):
+            return True
         else:
             return False
