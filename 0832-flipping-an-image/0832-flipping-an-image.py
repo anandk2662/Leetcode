e3ee6e246cba3de0c14e0d47a231123bd64be7c0
anandk2662 <130994394+anandk2662@users.noreply.github.com>
2026-01-27 23:28:15 +0530

    Time: 0 ms (100%), Space: 12.4 MB (91.91%) - LeetHub

---
+++ 0832-flipping-an-image/0832-flipping-an-image.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def flipAndInvertImage(self, image):
+        """
+        :type image: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        return [[abs(x-1) for x in row[::-1]] for row in image]
