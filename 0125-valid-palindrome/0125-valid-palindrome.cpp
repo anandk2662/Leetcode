0c27302d869e713a0bf7dfa2aa4a2b5aa7541e1b
anandk2662 <130994394+anandk2662@users.noreply.github.com>
2026-08-14 19:41:07 +0530

    Time: 0 ms (100%), Space: 9.7 MB (99.62%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    bool isAlphanum(char ch){
-        if((ch>='0' && ch<='9') ||
-         (tolower(ch)>='a' && tolower(ch)<='z')){
+    bool isAlphaNum(char c){
+        if((c>='0' && c<='9') || tolower(c)>='a' && tolower(c)<='z'){
             return true;
-        }return false;
+        }
+        return false;
     }
     bool isPalindrome(string s) {
-       int st=0,end=s.length()-1;
-       while(st<end){
-        if(!isAlphanum(s[st])){
-            st++;continue;
-        }
-        if(!isAlphanum(s[end])){
-            end--;continue;
-        }
+        int st=0,end=s.length()-1;
+        while(st<end){
+        if(!(isAlphaNum(s[st]))){st++;continue;}
+        if(!isAlphaNum(s[end])) {end--;continue;}
         if(tolower(s[st])!=tolower(s[end])){
             return false;
-        } st++ ; end--;
-       }
-      return true;  
+        }
+        st++; end--;
+
+        }
+        return true;
     }
 };
